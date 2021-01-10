e709a9f5d3a025c2effb1bc2ca85a1d7cee9720b
iamsomraj <iamsomraj@gmail.com>
2021-01-10 19:53:10 +0530

    TTTUtil is updated with comments

--- src/com/psl/training/assignment/sma/TicTacToeUtil.java
+++ src/com/psl/training/assignment/sma/TicTacToeUtil.java
@@ -2,10 +2,33 @@ package com.psl.training.assignment.sma;
 
 import java.util.Scanner;
 
+/**
+ * Assignment Date 10.1.21 Create a two dimensional character array which will
+ * accept only two characters X or O. populate array with different combinations
+ * of X and O characters. If Same character appears at either diagonal position
+ * or in the same line, return that character.
+ * 
+ * For eg.
+ * 
+ * X O O
+ * 
+ * O X O
+ * 
+ * O O X
+ * 
+ * X appears at diagonal position hence return X.
+ * 
+ * 
+ * @author Somraj Mukherjee
+ *
+ */
 public class TicTacToeUtil {
 
 	public static char board[][] = new char[3][3];
 
+	/**
+	 * Initializes the board for play
+	 */
 	public static void init() {
 		System.out.println("Welcome to Somraj's Tic Tac Toe:");
 		for (int i = 0; i < board.length; i++) {
@@ -16,10 +39,24 @@ public class TicTacToeUtil {
 		}
 	}
 
+	/**
+	 * Checks if the given row and col is valid for board
+	 * 
+	 * @param i
+	 * @param j
+	 * @return boolean
+	 */
 	public static boolean isValid(int i, int j) {
 		return (i >= 0 && i <= 2) && (j >= 0 && j <= 2);
 	}
 
+	/**
+	 * Checks if the given row and col position is occupied or not
+	 * 
+	 * @param i
+	 * @param j
+	 * @return boolean
+	 */
 	public static boolean isOccupied(int i, int j) {
 		if (board[i][j] != '-') {
 			return true;
@@ -27,14 +64,31 @@ public class TicTacToeUtil {
 		return false;
 	}
 
+	/**
+	 * Helper function to take the input of O
+	 * 
+	 * @param i
+	 * @param j
+	 */
 	public static void takeInputO(int i, int j) {
 		board[i][j] = 'O';
 	}
 
+	/**
+	 * Helper function to take the input of X
+	 * 
+	 * @param i
+	 * @param j
+	 */
 	public static void takeInputX(int i, int j) {
 		board[i][j] = 'X';
 	}
 
+	/**
+	 * Takes row and col input from the user
+	 * 
+	 * @param symbol
+	 */
 	public static void takeInput(char symbol) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Turn for " + symbol + ": ");
@@ -42,7 +96,7 @@ public class TicTacToeUtil {
 		int row = sc.nextInt();
 		System.out.println("Enter col:");
 		int col = sc.nextInt();
-		if(!isValid(row, col)) {
+		if (!isValid(row, col)) {
 			System.out.println("Position is invalid!");
 			takeInput(symbol);
 		} else {
@@ -65,6 +119,14 @@ public class TicTacToeUtil {
 		}
 	}
 
+	/**
+	 * Checks if the game is already won or not
+	 * 
+	 * @param symbol
+	 * @param currentRow
+	 * @param currentCol
+	 * @return boolean
+	 */
 	public static boolean hasWon(char symbol, int currentRow, int currentCol) {
 		return ((board[currentRow][0] == symbol && board[currentRow][1] == symbol && board[currentRow][2] == symbol)
 				|| (board[0][currentCol] == symbol && board[1][currentCol] == symbol && board[2][currentCol] == symbol)
@@ -74,6 +136,9 @@ public class TicTacToeUtil {
 						&& board[2][0] == symbol));
 	}
 
+	/**
+	 * The game cycle function which controls the turns
+	 */
 	public static void gameCycle() {
 		init();
 		for (int i = 0; i < 9; i++) {
@@ -86,8 +151,12 @@ public class TicTacToeUtil {
 		}
 		System.out.println("This is a tie!");
 		System.out.println("Thanks for playing!");
+		return;
 	}
 
+	/**
+	 * Displays the board
+	 */
 	public static void display() {
 		System.out.println("The board:\n");
 		for (int i = 0; i < board.length; i++) {
