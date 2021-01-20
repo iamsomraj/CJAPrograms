a02b74f27c46ee50f52e958770bc5641fe1281aa
iamsomraj <iamsomraj@gmail.com>
2021-01-20 12:56:23 +0530

    Employee Collection is updated

--- src/com/psl/training/assignment/empcoll/EmployeeService.java
+++ src/com/psl/training/assignment/empcoll/EmployeeService.java
@@ -7,12 +7,16 @@ import java.util.List;
 public class EmployeeService {
 	private List<Employee> employeeList = new ArrayList<Employee>();
 
+	public int generateEmployeeId() {
+		return (int) (Math.floor(Math.random() * 1000) + 1);
+	}
+
 	public EmployeeService() {
-		employeeList.add(new Employee(100, "Somraj", "a", "a", Date.valueOf("2020-12-16")));
-		employeeList.add(new Employee(101, "Barshan", "b", "b", Date.valueOf("2020-12-16")));
-		employeeList.add(new Employee(102, "Sayak", "c", "c", Date.valueOf("2020-12-16")));
-		employeeList.add(new Employee(103, "Dipanjan", "c", "c", Date.valueOf("2020-12-16")));
-		employeeList.add(new Employee(104, "Rest", "d", "d", Date.valueOf("2020-12-16")));
+		employeeList.add(new Employee(generateEmployeeId(), "Somraj", "a", "a", Date.valueOf("2020-12-16")));
+		employeeList.add(new Employee(generateEmployeeId(), "Barshan", "b", "b", Date.valueOf("2020-12-16")));
+		employeeList.add(new Employee(generateEmployeeId(), "Sayak", "c", "c", Date.valueOf("2020-12-16")));
+		employeeList.add(new Employee(generateEmployeeId(), "Dipanjan", "c", "c", Date.valueOf("2020-12-16")));
+		employeeList.add(new Employee(generateEmployeeId(), "Rest", "d", "d", Date.valueOf("2020-12-16")));
 	}
 
 	public List<Employee> getEmployeeList() {
--- src/com/psl/training/assignment/empcoll/EmployeeUtil.java
+++ src/com/psl/training/assignment/empcoll/EmployeeUtil.java
@@ -1,10 +1,20 @@
 package com.psl.training.assignment.empcoll;
 
 import java.sql.Date;
+import java.util.Comparator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+
+class EmpIdSorter implements Comparator<Employee> {
+
+	@Override
+	public int compare(Employee o1, Employee o2) {
+		return o1.getEmployeeId() - o2.getEmployeeId();
+	}
+
+}
 
 public class EmployeeUtil {
 
@@ -13,15 +23,25 @@ public class EmployeeUtil {
 		Employee somraj = new Employee(100, "Somraj", "a", "a", Date.valueOf("2020-12-16"));
 		System.out.println("The employees are: ");
 		System.out.println(employeeService);
+		System.out.println("The employee detail one by one: ");
+		for (Employee emp : employeeService.getEmployeeList()) {
+			System.out.println(emp);
+		}
+		System.out.println(employeeService);
 		List<Employee> employeeList = employeeService.getEmployeeList();
 		System.out.println("Is Somraj present? " + employeeList.contains(somraj));
 		employeeList.add(somraj);
 		System.out.println("Employee List Length: " + employeeList.size());
 		Set<Employee> empSet = new LinkedHashSet<Employee>(employeeList);
 		System.out.println("Employee Set Length: " + empSet.size());
-		Set<Employee> sortedEmpSet = new TreeSet<Employee>(employeeList);
-		System.out.println("Employee Sorted Set: ");
-		System.out.println(sortedEmpSet);
+		Set<Employee> sortedEmpIdSet = new TreeSet<Employee>(new EmpIdSorter());
+		sortedEmpIdSet.addAll(employeeList);
+		System.out.println("Employee Sorted Id Set: ");
+		System.out.println(sortedEmpIdSet);
+		Set<Employee> sortedEmpNameSet = new TreeSet<Employee>();
+		sortedEmpNameSet.addAll(employeeList);
+		System.out.println("Employee Sorted Name Set: ");
+		System.out.println(sortedEmpNameSet);
 	}
 
 }
